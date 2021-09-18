29d45846f0a0f05b110171ab08359c34b6c9b709
Angel D. Munoz <scythes.lol@gmail.com>
2021-09-18 12:19:59 -0500

    add messages to the counter

--- src/Components/Counter.fs
+++ src/Components/Counter.fs
@@ -1,6 +1,7 @@
 [<RequireQualifiedAccess>]
 module Components.Counter
 
+open Browser.Types
 open Lit
 open Haunted
 
@@ -8,12 +9,34 @@ let private counter (props: {| initial: int option |}) =
     let count, setCount =
         Haunted.useState (defaultArg props.initial 0)
 
+    let messageTpl =
+        match count with
+        | count when count > 100 ->
+            html
+                $"""
+                <fs-message header="Danger high count!" kind="danger" is-open>
+                    <p>I don't want to be that guy but... that's a high count!</p>
+                </fs-message>
+                """
+        | count when count < 0 ->
+            html
+                $"""
+                <fs-message header="Warning low count!" kind="warning" is-open>
+                    <p>I don't want to be that guy but... that's a low count!</p>
+                </fs-message>
+                """
+        | _ -> Lit.nothing
+
+
     html
         $"""
         <p>Home: {count}</p>
         <button @click={fun _ -> setCount (count + 1)}>Increment</button>
         <button @click={fun _ -> setCount (count - 1)}>Decrement</button>
         <button @click={fun _ -> setCount (defaultArg props.initial 0)}>Reset</button>
+        <div @fs-close-message={fun (e: Event) -> (e.target :?> HTMLElement).remove ()}>
+            {messageTpl}
+        </div>
         """
 
 let register () =
